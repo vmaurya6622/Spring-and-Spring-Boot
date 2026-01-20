58e8a7769da311f4200ab3e3068a93359b200e5e
Vishal Kumar Maurya <118094180+vmaurya6622@users.noreply.github.com>
2026-01-20 12:06:23 +0530

    done

--- Sprint_Boot_Learning/src/main/java/org/example/Ioc_DI/Ioc_Di.java
+++ Sprint_Boot_Learning/src/main/java/org/example/Ioc_DI/Ioc_Di.java
@@ -1,8 +1,13 @@
 package org.example.Ioc_DI;
 /*
-    -> IoC is a design principle in software design where traditional flow is reversed.
-    -> In here, control of object creation is handed over to a framework or container.
-    ->
+    -> Inversion of control (IoC)
+        -> IoC is a design principle in software design where traditional flow is reversed.
+        -> In here, control of object creation is handed over to a framework or container.
+        -> it improves modularity and flexibility which helps the maintainable and testable code.
+    -> Dependency Injection
+        -> DI is the design patter  used to implement IoC. It involves injecting dependencies into a class
+           rather than the class creating them itself.
+        ->
 */
 
 public class Ioc_Di {
--- Sprint_Boot_Learning/src/main/java/org/example/demo/HelloController.java
+++ Sprint_Boot_Learning/src/main/java/org/example/demo/HelloController.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
