0b8a7f58fcd3dc4b868289b825d45047a1dd9b02
Vishal Kumar Maurya <118094180+vmaurya6622@users.noreply.github.com>
2026-01-20 10:29:03 +0530

    Done

---
+++ Sprint_Boot_Learning/src/main/java/org/example/Introduction/Intro.java
@@ -0,0 +1,20 @@
+package org.example.Introduction;
+/*
+    -> Spring is a comprehensive Java framework for developing Industry standard, robust and high grade applications.
+    -> it provides a complete solution for all application needs like: web, database, security etc.
+    Features:
+         -> it build applications using POJO plain old java Objects.
+         -> Handles creation and dependency management.
+         -> Manages cross-cutting concerns rather than other business logics.
+    Spring Modules:
+         -> Spring core.
+         -> Spring AOP.
+         -> Spring ORM.
+         -> Spring MVC.
+         -> Spring Boot.
+    -> In all spring is lightweight and modular which promotes clean and modular maintainable code.
+       due to which it has become standard for java development.
+
+*/
+public class Intro {
+}
---
+++ Sprint_Boot_Learning/src/main/java/org/example/Ioc_DI/Ioc_Di.java
@@ -0,0 +1,7 @@
+package org.example.Ioc_DI;
+/*
+    -> IoC is a broad principle in software design where traditional flow is reversed.
+    -> 
+*/
+public class Ioc_Di {
+}
